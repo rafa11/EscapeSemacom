0d61e0ae4dffbd40c103c7f80f078e96c4b14e9b
rafa11 <new_day_system@live.com.ar>
2014-11-08 01:41:34 -0600

    segundo comit

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8" />
+	<meta name =="viewport" content="width=device">
+	<title>EscapeSemacom</title>
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/skeleton.css">
+	<link rel="stylesheet" href="css/layout.css">
+	<link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico" />
+	<link href='http://fonts.googleapis.com/css?family=Raleway:400,900,800,700,600,500,300,200,100' rel='stylesheet' type='text/css'>
+
+	<script src="js/jquery-1.8.0.min.js" type="text/javascript"></script>
+	<script src="js/functions.js" type="text/javascript"></script>
+	<script src="js/jquery.carouFredSel-5.5.0-packed.js" type="text/javascript"></script>
+</head>
+
+<body>
+<div class="container">
+
+	<!-- header -->
+	<header id="header">
+		<div class="logo">
+			<h1 class="titular">Escape Semacom</h1>
+		</div>
+		
+	</header>
+	<!--termina header-->
+
+	<!--nav -->
+	<nav>
+        <div class="menu">
+            <ul>
+                <li class="active"><a href="#">Inicio</a></li>
+                <li><a href="#">Quienes somos</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Promociones</a></li>
+                <li><a href="#">Servicio</a></li>
+                <li><a href="#">Consulta</a></li>
+                <li><a href="#">Contactanos</a></li>
+            </ul>
+        </div>
+    </nav>
+	<!-- Termina nav -->
+
+	<!-- section -->
+	<section class="posts">
+		<article class="post">
+			<div class="descripcion">
+				<figure class="imagen">
+					<img src="imagenes/foto.jpg" alt="fotopost">
+				</figure>
+				<div class="detalles">
+					<h2 class="titulo">Jesus es un programador</h2>
+					<p class="autor">por <a href="#" />Carlos jimenez</a></p>
+					<a href="#" class="tag" />PHP</a>
+					<p class="fecha">hace <strong>30</strong> min</p>
+				</div>
+			</div>
+			<div class="acciones">
+				<div class="votos">
+					<a href="#" class="up">up</a>
+					<span class="total">167</span>
+					<a href="#" class="down">down</a>
+				</div>
+				<div class="datos">
+					<a href="#" class="comentarios">10</a>
+					<a href="#" class="estrellita"></a>
+				</div>
+
+
+			</div>
+
+		</article>
+	
+	</section>
+	<!-- termina section -->
+
+	<!--footer -->
+	<footer>
+		<h3 class="powered">
+			<strong>
+				Powered by Jesús
+			</strong>
+			<span class="rayita">
+				Aprendiendo Fron-end
+			</span>
+		</h3>
+	</footer>
+	<!--termina footer
+	<!-- termina header -->
+</div>
+</body>
+</html>
